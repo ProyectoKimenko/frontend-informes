b2d79cbe0a6709ec265ab20fff0694ab3328a482
FelipeLara00 <felipe.lara@uc.cl>
2024-08-14 12:13:27 -0400

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,70 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+
+export default function LogIn() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [hidePassword, setHidePassword] = useState(true);
+  return (
+    <main 
+      className="flex min-h-screen flex-col items-center justify-center bg-gray-100 p-1"
+    >
+      <div
+        className="flex flex-col sm:flex-row justify-between w-full sm:w-2/3 2xl:w-1/2 bg-white p-5 sm:py-16 sm:px-10 shadow-sm"
+      >
+        <div
+          className="flex flex-col w-1/3"
+        >
+          <Image
+            src={"/logo-kimenko.jpg"}
+            width={130}
+            height={130}
+            alt="Logo Kimenko"
+          />
+        </div>
+        <div
+          className="flex flex-col w-full sm:w-1/2 sm:border-l p-3 mt-3 sm:mt-0"
+        >
+          <input
+            value={username}
+            onChange={(e)=>setUsername(e.target.value)}
+            className="border h-10 p-2 focus:outline-lightBlue focus:rounded-none w-full"
+            placeholder="Nombre usuario"
+          />
+          <div
+            className="relative w-full"
+          >
+            <input
+              value={password}
+              onChange={(e)=>setPassword(e.target.value)}
+              className="border h-10 p-2 focus:outline-lightBlue focus:rounded-none mt-3 w-full"
+              placeholder="Contraseña"
+              type={
+                hidePassword
+                  ? "password"
+                  : "text"
+              }
+            />
+          </div>
+          <div
+            className="flex items-center justify-between mt-3 w-full"
+          > 
+            <Link
+              href={"/"}
+              className="text-xs text-gray-600 hover:text-lightBlue"
+            >
+              ¿Olvidaste tu contraseña?
+            </Link>
+            <button
+              className="bg-lightBlue w-fit px-4 py-1 text-white place-self-end shadow hover:bg-cyan-400"
+            >
+              Ingresar
+            </button>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
